c1994a90e33dbe49a2878ab10cdd8a1170f38d30
Felipe <solanoluz@gmail.com>
2017-04-05 11:16:10 -0300

    detect_dependencies to false / adding depenencies manually

--- lib/elevate.rb
+++ lib/elevate.rb
@@ -5,7 +5,9 @@ unless defined?(Motion::Project::Config)
 end
 
 Motion::Project::App.setup do |app|
+  app.detect_dependencies = false
   Dir.glob(File.join(File.dirname(__FILE__), "elevate/**/*.rb")).each do |file|
     app.files.unshift(file)
   end
+  app.files_dependencies File.join(File.dirname(__FILE__), "elevate/http.rb") => File.join(File.dirname(__FILE__), "elevate/http/request.rb")
 end
